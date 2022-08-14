831676a1dc0d5293a5643c4a94664855c250942c
Jáchym Křeček <krecejac@fit.cvut.cz>
2022-08-14 21:15:52 +0200

    Solving error

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 20 ? 'warm' : ''">
     <main>
       <div class="search-box">
         <input 
-          type="text"
-          class="search-bar"
-          placeholder="Search..." 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
           v-model="query"
           @keypress="fetchWeather"
         />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">North Hampton, UK</div>
-          <div class="date">Monday 20 January 2020</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
+
         <div class="weather-box">
-          <div class="temp">9°</div>
-            <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
-      
     </main>
   </div>
 </template>
@@ -31,7 +31,7 @@ export default {
   name: 'App',
   data () {
     return {
-      api_key: 'ed942279b42b20c1a43ab61fef12375d',
+      api_key: '770187d780488415ba22eacbbf4ac2d1',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {}
@@ -39,9 +39,31 @@ export default {
   },
   methods: {
     fetchWeather () {
-      if( e.key == "")
+      if( e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July",
+      "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+      "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     }
-  },
+  }
 }
 </script>
 
@@ -61,6 +83,10 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url('./assets/warm-bg.jpg');
 }
 
 main {
